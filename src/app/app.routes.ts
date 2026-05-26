d64d7767454d6fa67b55ace6da955e82e652838f
Priyanshu Khakkhar <khakkharpriyanshu@gmail.com>
2026-05-26 16:40:34 +0530

    Configured application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login.component')
+        .then(m => m.LoginComponent)
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./features/dashboard/dashboard/dashboard.component')
+        .then(m => m.DashboardComponent)
+  },
+  {
+    path: 'board',
+    loadComponent: () =>
+      import('./features/board/kanban-board/kanban-board.component')
+        .then(m => m.KanbanBoardComponent)
+  }
+];
